da6d559b3c80bc87191e1cc336f9390452d00973
Crowdbotics <engineering@crowdbotics.com>
2023-04-19 01:22:13 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "throbbing-band-dev-77487",
+  url: "https://throbbing-band-dev-77487.botics.co",
+  api: "https://throbbing-band-dev-77487.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
